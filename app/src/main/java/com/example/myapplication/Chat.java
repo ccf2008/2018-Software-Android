69befb38076ec86f4f42e4a6f1bf5b8249497949
ccf2008 <40535517+ccf2008@users.noreply.github.com>
2018-06-24 18:21:22 +0900

    Update Chat.java

--- app/src/main/java/com/example/myapplication/Chat.java
+++ app/src/main/java/com/example/myapplication/Chat.java
@@ -4,7 +4,7 @@ package com.example.myapplication;
  * Created by 이진재 on 2018-04-24.
  */
 
-public class Chat {
+public class Chat {  // 채팅 정보를 담을 클래스
 
     public String email;
     public String text;
